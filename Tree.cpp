23da845fb9342c469fe9f2449c781e8896578a99
DinLei <dinglei_1107@outlook.com>
2020-12-13 22:51:57 +0800

    new

--- Tree.cpp
+++ Tree.cpp
@@ -144,3 +144,306 @@ public:
     swap(e1->val, e2->val);
   }
 };
+
+/*
+105. 从前序与中序遍历序列构造二叉树
+106. 从中序与后序遍历序列构造二叉树
+*/
+
+/*
+根据一棵树的前序遍历与中序遍历构造二叉树。
+*/
+// 递归：关键是确定根的位置之后，左右子树元素个数相等
+class Solution {
+private:
+    unordered_map<int, int> index;
+
+public:
+    TreeNode* myBuildTree(const vector<int>& preorder, const vector<int>& inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+        if (preorder_left > preorder_right) {
+            return nullptr;
+        }
+        
+        // 前序遍历中的第一个节点就是根节点
+        int preorder_root = preorder_left;
+        // 在中序遍历中定位根节点
+        int inorder_root = index[preorder[preorder_root]];
+        
+        // 先把根节点建立出来
+        TreeNode* root = new TreeNode(preorder[preorder_root]);
+        // 得到左子树中的节点数目
+        int size_left_subtree = inorder_root - inorder_left;
+        // 递归地构造左子树，并连接到根节点
+        // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
+        root->left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
+        // 递归地构造右子树，并连接到根节点
+        // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+        root->right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = preorder.size();
+        // 构造哈希映射，帮助我们快速定位根节点
+        for (int i = 0; i < n; ++i) {
+            index[inorder[i]] = i;
+        }
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+};
+
+// 迭代
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if (!preorder.size()) {
+            return nullptr;
+        }
+        TreeNode* root = new TreeNode(preorder[0]);
+        stack<TreeNode*> stk;
+        stk.push(root);
+        int inorderIndex = 0;
+        for (int i = 1; i < preorder.size(); ++i) {
+            int preorderVal = preorder[i];
+            TreeNode* node = stk.top();
+            if (node->val != inorder[inorderIndex]) {
+                node->left = new TreeNode(preorderVal);
+                stk.push(node->left);
+            }
+            else {
+                while (!stk.empty() && stk.top()->val == inorder[inorderIndex]) {
+                    node = stk.top();
+                    stk.pop();
+                    ++inorderIndex;
+                }
+                node->right = new TreeNode(preorderVal);
+                stk.push(node->right);
+            }
+        }
+        return root;
+    }
+};
+
+/*110. 平衡二叉树*/
+/*
+给定一个二叉树，判断它是否是高度平衡的二叉树。
+
+本题中，一棵高度平衡二叉树定义为：一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+*/
+// 本题的关键是，在递归计算子树高度的时候，就递归的进行判断，如果不满足就返回-1
+class Solution {
+public:
+  int height(TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    } 
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+    if(leftHeight == -1 || rightHeight == -1 || abs(leftHeight-rightHeight) > 1)
+      return -1;
+    return max(leftHeight, rightHeight) + 1;
+  }
+
+  bool isBalanced(TreeNode* root) {
+    if(root == NULL)
+      return 1;
+    return height(root) > 0;
+  }
+};
+
+/*112. 路径总和*/
+/*
+给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+*/
+class Solution {
+public:
+  bool hasPathSum(TreeNode* root, int sum) {
+    if( root == NULL )
+      return false;
+    if( root->left == NULL && root->right == NULL )
+      return sum == root->val;
+    return hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);
+  }
+};
+
+/*114. 二叉树展开为链表*/
+/*
+给定一个二叉树，原地将它展开为一个单链表。
+*/
+//方法一：前序遍历和展开同步进行。关键是将父节点保存下来，左右子节点压栈，先右后左
+// 树 和 链表 一样，只要想办法把指针指对了就行。指针对应信息序列内部会保存下来。
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if (root == nullptr) {
+            return;
+        }
+        auto stk = stack<TreeNode*>();
+        stk.push(root);
+        TreeNode *prev = nullptr;
+        while (!stk.empty()) {
+            TreeNode *curr = stk.top(); stk.pop();
+            if (prev != nullptr) {
+                prev->left = nullptr;
+                prev->right = curr;
+            }
+            TreeNode *left = curr->left, *right = curr->right;
+            if (right != nullptr) {
+                stk.push(right);
+            }
+            if (left != nullptr) {
+                stk.push(left);
+            }
+            prev = curr;
+        }
+    }
+};
+
+//方法二：寻找前驱节点。解题的关键是：左子树的最下最右的节点，是右子树的父节点.
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode *curr = root;
+        while (curr != nullptr) {
+            if (curr->left != nullptr) {
+                auto next = curr->left;
+                auto predecessor = next;
+                while (predecessor->right != nullptr) {
+                    predecessor = predecessor->right;
+                }
+                predecessor->right = curr->right;
+                curr->left = nullptr;
+                curr->right = next;
+            }
+            curr = curr->right;
+        }
+    }
+};
+
+/*116. 填充每个节点的下一个右侧节点指针*/
+/*
+给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+
+初始状态下，所有 next 指针都被设置为 NULL。
+
+进阶：
+
+你只能使用常量级额外空间。
+使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+// 方法一：层次遍历 O(n) + O(n)
+// 方法二：使用已建立的 next 指针
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (root == nullptr) {
+            return root;
+        }
+        
+        // 从根节点开始
+        Node* leftmost = root;
+        
+        while (leftmost->left != nullptr) {
+            
+            // 遍历这一层节点组织成的链表，为下一层的节点更新 next 指针
+            Node* head = leftmost;
+            
+            while (head != nullptr) {
+                
+                // CONNECTION 1
+                head->left->next = head->right;
+                
+                // CONNECTION 2
+                if (head->next != nullptr) {
+                    head->right->next = head->next->left;
+                }
+                
+                // 指针向后移动
+                head = head->next;
+            }
+            
+            // 去下一层的最左的节点
+            leftmost = leftmost->left;
+        }
+        
+        return root;
+    }
+};
+/*117. 填充每个节点的下一个右侧节点指针 II*/
+// 注意引用的问题。如果子逻辑太多，考虑子逻辑拿出来做一个函数
+class Solution {
+public:
+    void handle(Node* &last, Node* &p, Node* &nextStart) {
+        if (last) {
+            last->next = p;
+        } 
+        if (!nextStart) {
+            nextStart = p;
+        }
+        last = p;
+    }
+
+    Node* connect(Node* root) {
+        if (!root) {
+            return nullptr;
+        }
+        Node *start = root;
+        while (start) {
+            Node *last = nullptr, *nextStart = nullptr;
+            for (Node *p = start; p != nullptr; p = p->next) {
+                if (p->left) {
+                    handle(last, p->left, nextStart);
+                }
+                if (p->right) {
+                    handle(last, p->right, nextStart);
+                }
+            }
+            start = nextStart;
+        }
+        return root;
+    }
+};
+
+
+/*124. 二叉树中的最大路径和*/
+/*
+给定一个非空二叉树，返回其最大路径和。
+
+本题中，路径被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。该路径至少包含一个节点，且不一定经过根节点。
+*/
+class Solution {
+private:
+    int maxSum = INT_MIN;
+
+public:
+    int maxGain(TreeNode* node) {
+        if (node == nullptr) {
+            return 0;
+        }
+        
+        // 递归计算左右子节点的最大贡献值
+        // 只有在最大贡献值大于 0 时，才会选取对应子节点
+        int leftGain = max(maxGain(node->left), 0);
+        int rightGain = max(maxGain(node->right), 0);
+
+        // 节点的最大路径和取决于该节点的值与该节点的左右子节点的最大贡献值
+        int priceNewpath = node->val + leftGain + rightGain;
+
+        // 更新答案
+        maxSum = max(maxSum, priceNewpath);
+
+        // 返回节点的最大贡献值
+        return node->val + max(leftGain, rightGain);
+    }
+
+    int maxPathSum(TreeNode* root) {
+        maxGain(root);
+        return maxSum;
+    }
+};
