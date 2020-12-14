bb69056b627c2411a91da910845d93641afa0af3
bading <dinglei_1107@outlook.com>
2020-12-14 20:03:34 +0800

    new

--- DynamicProgram.cpp
+++ DynamicProgram.cpp
@@ -1,4 +1,56 @@
 // 动态规划
+/*5. 最长回文子串*/
+/*给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。*/
+// 方法一：动态规划
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+        string ans;
+        for (int l = 0; l < n; ++l) {
+            for (int i = 0; i + l < n; ++i) {
+                int j = i + l;
+                if (l == 0) {
+                    dp[i][j] = 1;
+                } else if (l == 1) {
+                    dp[i][j] = (s[i] == s[j]);
+                } else {
+                    dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]);
+                }
+                if (dp[i][j] && l + 1 > ans.size()) {
+                    ans = s.substr(i, l + 1);
+                }
+            }
+        }
+        return ans;
+    }
+};
+// 方向二：中心扩展
+/**/
+/*516. 最长回文子序列*/
+int longestPalindromeSubseq(string s) {
+    int n = s.size();
+    // dp 数组全部初始化为 0
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+    // base case
+    for (int i = 0; i < n; i++)
+        dp[i][i] = 1;
+    // 反着遍历保证正确的状态转移
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = i + 1; j < n; j++) {
+            // 状态转移方程
+            if (s[i] == s[j])
+                dp[i][j] = dp[i + 1][j - 1] + 2;
+            else
+                dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+        }
+    }
+    // 整个 s 的最长回文子串长度
+    return dp[0][n - 1];
+}
+
+
 /*
 376. 摆动序列
 level: 中等
@@ -42,4 +94,34 @@ public:
     }
 };
 
-// 方法二: 二分查找
+// 方法二: 二分查找。注：二分查找可以返回最长的上升序列
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int len = 1, n = (int)nums.size();
+        if (n == 0) {
+            return 0;
+        }
+        vector<int> d(n + 1, 0);
+        d[len] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                int l = 1, r = len, pos = 0; // 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (d[mid] < nums[i]) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                d[pos + 1] = nums[i];
+            }
+        }
+        return len;
+    }
+};
+
