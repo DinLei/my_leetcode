fcb6ec84ceed78f75dd168009e2def69c95053f6
bading <dinglei_1107@outlook.com>
2020-12-23 19:55:16 +0800

    new

--- Array_Matrix_Pointer.cpp
+++ Array_Matrix_Pointer.cpp
@@ -342,6 +342,7 @@ public:
     }
   }
 };
+
 /*=========================================================================================================================================*/
 
 
@@ -515,3 +516,50 @@ public:
 // 链接：https://leetcode-cn.com/problems/fraction-to-recurring-decimal/solution/zhong-gui-zhong-ju-xi-jie-kao-cha-ti-by-jyj407/
 // 来源：力扣（LeetCode）
 // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+/*842. 将数组拆分成斐波那契序列*/
+/*
+给定一个数字字符串 S，比如 S = "123456579"，我们可以将它分成斐波那契式的序列 [123, 456, 579]。
+
+形式上，斐波那契式序列是一个非负整数列表 F，且满足：
+
+0 <= F[i] <= 2^31 - 1，（也就是说，每个整数都符合 32 位有符号整数类型）；
+F.length >= 3；
+对于所有的0 <= i < F.length - 2，都有 F[i] + F[i+1] = F[i+2] 成立。
+另外，请注意，将字符串拆分成小块时，每个块的数字一定不要以零开头，除非这个块是数字 0 本身。
+
+返回从 S 拆分出来的任意一组斐波那契式的序列块，如果不能拆分则返回 []。
+*/
+class Solution {
+private:
+  vector<int> ans;
+public:
+  vector<int> splitIntoFibonacci(string S) {
+    backTracking(S, 0, 0, 0);
+    return ans;
+  }
+
+  bool backTracking(string S, int idx, long long sum, long long prev) {
+    if(idx >= S.size()) {
+      return ans.size() >= 3;
+    }
+    long long curr = 0;
+    for(int i = idx; i < S.size(); i ++) {
+      // 可以是0本身，即 S[idx]=0, 但要排除0开头
+      if(i > idx && S[idx] == '0') break;
+      curr = curr * 10 + (S[i] - '0');
+      // curr 需要等于前两个数的和
+      if(curr > INT_MAX) break;
+      if(ans.size() >= 2) {
+        if(sum > curr) continue;
+        else if(sum < curr) break;
+      }
+      ans.push_back(curr);
+      if(backTracking(S, i + 1, prev + curr, curr)) 
+        return true;
+      ans.pop_back();
+    }
+    return false;
+  }
+};
