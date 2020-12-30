9856c68715b30d51f7697fa2fadf4a35c982f648
bading <dinglei_1107@outlook.com>
2020-12-30 11:54:10 +0800

    new

--- List.cpp
+++ List.cpp
@@ -449,3 +449,119 @@ public:
     inorder(root -> right); // 右
   }
 };
+
+
+
+/*148. 排序链表*/
+/*
+给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+
+进阶：
+你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+*/
+// 方法一：自顶向下归并排序
+// 时间复杂度：O(nlogn)，其中 n 是链表的长度
+// 空间复杂度：O(logn)， 其中 n 是链表的长度。空间复杂度主要取决于递归调用的栈空间。
+  ListNode* sortList(ListNode* node) {
+    if(!node || !node->next)
+      return node;
+    ListNode* slow = node, * fast = node, * split;
+    while(fast && fast->next) {
+      split = slow;
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+    split->next = nullptr;
+    ListNode* curr = node, * next = slow;
+    curr = sortList(curr); next = sortList(next);
+    return mergeTwoList(curr, next);
+  }
+
+  ListNode* mergeTwoList(ListNode* l1, ListNode* l2) {
+    if(!l1) return l2;
+    if(!l2) return l1;
+    if(l1->val < l2->val) {
+      l1->next = mergeTwoList(l1->next, l2);
+      return l1;
+    } else {
+      l2->next = mergeTwoList(l1, l2->next);
+      return l2;
+    }
+  }
+
+/*
+方法二：自底向上归并排序
+时间复杂度：O(nlogn)，其中 n 是链表的长度。
+空间复杂度：O(1)。
+*/
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (head == nullptr) {
+            return head;
+        }
+        // 求该链表的总长度
+        int length = 0;
+        ListNode* node = head;
+        while (node != nullptr) {
+            length++;
+            node = node->next;
+        }
+        ListNode* dummyHead = new ListNode(0, head);
+        for (int subLength = 1; subLength < length; subLength <<= 1) {
+            ListNode* prev = dummyHead, *curr = dummyHead->next;
+            while (curr != nullptr) {
+                // 切割 第一部分 子数组
+                ListNode* head1 = curr;
+                for (int i = 1; i < subLength && curr->next != nullptr; i++) {
+                    curr = curr->next;
+                }
+                // 切割 第二部分 子数组
+                ListNode* head2 = curr->next;
+                curr->next = nullptr;
+                curr = head2;
+                // 注意第二部分 子数组切割的判断条件
+                for (int i = 1; i < subLength && curr != nullptr && curr->next != nullptr; i++) {
+                    curr = curr->next;
+                }
+                // 开启第三第四...部分
+                ListNode* next = nullptr;
+                if (curr != nullptr) {
+                    next = curr->next;
+                    curr->next = nullptr;
+                }
+                // 合并前两个
+                ListNode* merged = merge(head1, head2);
+                // 更新prev到新指针
+                prev->next = merged;
+                while (prev->next != nullptr) {
+                    prev = prev->next;
+                }
+                // 更新curr到新指针
+                curr = next;
+            }
+        }
+        return dummyHead->next;
+    }
+    // 合并双链表的非迭代写法
+    ListNode* merge(ListNode* head1, ListNode* head2) {
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* temp = dummyHead, *temp1 = head1, *temp2 = head2;
+        while (temp1 != nullptr && temp2 != nullptr) {
+            if (temp1->val <= temp2->val) {
+                temp->next = temp1;
+                temp1 = temp1->next;
+            } else {
+                temp->next = temp2;
+                temp2 = temp2->next;
+            }
+            temp = temp->next;
+        }
+        if (temp1 != nullptr) {
+            temp->next = temp1;
+        } else if (temp2 != nullptr) {
+            temp->next = temp2;
+        }
+        return dummyHead->next;
+    }
+};
--- Tree.cpp
+++ Tree.cpp
@@ -695,38 +695,39 @@ public:
 */
 
 /*
-链接：https://www.nowcoder.com/questionTerminal/9023a0c988684a53960365b889ceaf5e?answerType=1&f=discussion
-来源：牛客网
-
 可以把中序下一结点归为几种类型：
-有右子树，下一结点是右子树中的最左结点，例如 B，下一结点是 H
-无右子树，且结点是该结点父结点的左子树，则下一结点是该结点的父结点，例如 H，下一结点是 E
-无右子树，且结点是该结点父结点的右子树，则我们一直沿着父结点追朔，直到找到某个结点是其父结点的左子树，如果存在这样的结点，那么这个结点的父结点就是我们要找的下一结点。例如 I，下一结点是 A；例如 G，并没有符合情况的结点，所以 G 没有下一结点
+1. 有右子树，下一结点是右子树中的最左结点，例如 B，下一结点是 H
+2. 无右子树，且结点是该结点父结点的左子树，则下一结点是该结点的父结点，例如 H，下一结点是 E
+3. 无右子树，且结点是该结点父结点的右子树，则我们一直沿着父结点追朔，直到找到某个结点是其父结点的左子树，如果存在这样的结点，那么这个结点的父结点就是我们要找的下一结点。例如 I，下一结点是 A；例如 G，并没有符合情况的结点，所以 G 没有下一结点
 */
 
-public class Solution {
+class Solution {
+public:
+    TreeLinkNode* GetNext(TreeLinkNode* pNode) {
+        if (!pNode) {
+            return pNode;
+        }
  
-public TreeLinkNode GetNext(TreeLinkNode pNode) {
-     // 1.
-     if (pNode.right != null) {
-         TreeLinkNode pRight = pNode.right;
-         while (pRight.left != null) {
-             pRight = pRight.left;
-         }
-         return pRight;
-     }
-     // 2.
-     if (pNode.next != null && pNode.next.left == pNode) {
-         return pNode.next;
-     }
-     // 3.
-     if (pNode.next != null) {
-         TreeLinkNode pNext = pNode.next;
-         while (pNext.next != null && pNext.next.right == pNext) {
-             pNext = pNext.next;
-         }
-         return pNext.next;
-     }
-     return null;
- }
-}
+        // 1
+        if (pNode->right) {
+            pNode = pNode->right;
+            while (pNode->left) {
+                pNode = pNode->left;
+            }
+            return pNode;
+        }
+ 
+        // 2\3
+        while (pNode->next) {
+            // pNode的父节点
+            TreeLinkNode *root = pNode->next;
+            if (root->left == pNode) {
+                return root;
+            }
+            // 继续往更上层的父节点追
+            pNode = pNode->next;
+        }
+ 
+        return nullptr;
+    }
+};
