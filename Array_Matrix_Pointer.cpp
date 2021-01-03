9b4b8900ae8cc36df6fda4f89d70c7df029d014d
bading <dinglei_1107@outlook.com>
2021-01-03 23:19:56 +0800

    matrix search

--- Array_Matrix_Pointer.cpp
+++ Array_Matrix_Pointer.cpp
@@ -193,19 +193,17 @@ class Finder {
 public:
     bool findX(vector<vector<int> > mat, int n, int m, int x) {
         // write code here
-        // int curr_val = mat[0][m-1];
         int i = 0, j = m - 1;
         while(i < n && j >= 0){
             if(x > mat[i][j]){
                 ++ i;
-            }else if(x < mat[i][j]){
+            } else if(x < mat[i][j]){
                 -- j;
-            }
-            else{
-                return true;
+            } else {
+                return true;  // return {i, j};
             }
         }
-        return false;
+        return false; // return {};
     }
 };
 
