4da483badf01897a7fd3fa4e01981ca2a673d69e
bading <dinglei_1107@outlook.com>
2021-01-11 11:00:49 +0800

    new

--- Array_Matrix_Pointer.cpp
+++ Array_Matrix_Pointer.cpp
@@ -289,6 +289,68 @@ public:
 };
 
 
+/*===================================================通过横纵索引对矩阵进行旋转=====================================================================*/
+/*剑指 Offer 29. 顺时针打印矩阵*/
+/*输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。*/
+class Solution {
+public:
+  vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    int nrow = matrix.size();
+    if(nrow == 0) return vector<int>();
+    int ncol = matrix[0].size();
+    if(ncol == 0) return vector<int>();
+    int t = 0, b = nrow - 1, l = 0, r = ncol - 1, x = 0;
+    vector<int> ans(nrow * ncol);
+    while(1) {
+      // 从左往右
+      for(int i = l; i <= r; i ++) ans[x ++] = matrix[t][i];
+      // 最上层遍历完，下移
+      if(++ t > b) break;
+
+      // 从上往下
+      for(int i = t; i <= b; i ++) ans[x ++] = matrix[i][r];
+      // 最右侧遍历完，左移
+      if(l > -- r) break;
+
+      // 从右往左
+      for(int i = r; i >= l; i --) ans[x ++] = matrix[b][i];
+      // 最下层遍历完，上移
+      if(t > -- b) break;
+
+      // 从下往上
+      for(int i = b; i >= t; i --) ans[x ++] = matrix[i][l];
+      // 最左侧遍历完，右移
+      if(++ l > r) break;
+    }
+    return ans;
+  }
+};
+
+
+/*48. 旋转图像*/
+/*给定一个 n × n 的二维矩阵表示一个图像。
+将图像顺时针旋转 90 度。
+
+说明：
+你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+*/
+class Solution {
+public:
+  void rotate(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    for (int i = 0; i < n / 2; ++i) {
+      for (int j = 0; j < (n + 1) / 2; ++j) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[n - j - 1][i];
+        matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+        matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+        matrix[j][n - i - 1] = temp;
+      }
+    }
+  }
+};
+
+
 /*===================================================构造排列组合并去重=====================================================================*/
 // 15题、16题都是二分查找，最重要的是学习如何对排列组合进行去重复
 /*15. 三数之和*/
@@ -481,42 +543,6 @@ public:
   }
 };
 
-
-/*剑指 Offer 29. 顺时针打印矩阵*/
-/*输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。*/
-class Solution {
-public:
-  vector<int> spiralOrder(vector<vector<int>>& matrix) {
-    int nrow = matrix.size();
-    if(nrow == 0) return vector<int>();
-    int ncol = matrix[0].size();
-    if(ncol == 0) return vector<int>();
-    int t = 0, b = nrow - 1, l = 0, r = ncol - 1, x = 0;
-    vector<int> ans(nrow * ncol);
-    while(1) {
-      // 从左往右
-      for(int i = l; i <= r; i ++) ans[x ++] = matrix[t][i];
-      // 最上层遍历完，下移
-      if(++ t > b) break;
-
-      // 从上往下
-      for(int i = t; i <= b; i ++) ans[x ++] = matrix[i][r];
-      // 最右侧遍历完，左移
-      if(l > -- r) break;
-
-      // 从右往左
-      for(int i = r; i >= l; i --) ans[x ++] = matrix[b][i];
-      // 最下层遍历完，上移
-      if(t > -- b) break;
-
-      // 从下往上
-      for(int i = b; i >= t; i --) ans[x ++] = matrix[i][l];
-      // 最左侧遍历完，右移
-      if(++ l > r) break;
-    }
-    return ans;
-  }
-};
 
 /*=========================================================================================================================================*/
 
