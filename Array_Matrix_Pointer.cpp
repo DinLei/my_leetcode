500409b8b67779903600c36f8ef1ba3ebc988618
bading <dinglei_1107@outlook.com>
2021-01-13 09:34:08 +0800

    new

--- Array_Matrix_Pointer.cpp
+++ Array_Matrix_Pointer.cpp
@@ -137,13 +137,13 @@ public:
     int l = 0, r = height.size() - 1;
     int ans = 0;
     while (l < r) {
-        int area = min(height[l], height[r]) * (r - l);
-        ans = max(ans, area);
-        if (height[l] <= height[r]) {
-            ++ l;
-        } else {
-            -- r;
-        }
+      int area = min(height[l], height[r]) * (r - l);
+      ans = max(ans, area);
+      if (height[l] <= height[r]) {
+          ++ l;
+      } else {
+          -- r;
+      }
     }
     return ans;
   }
