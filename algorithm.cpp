781191f23142f0092d06aa8ef089cffcc21e3a99
bading <dinglei_1107@outlook.com>
2021-01-19 19:55:25 +0800

    new

--- algorithm.cpp
+++ algorithm.cpp
@@ -100,3 +100,101 @@ long long fastPower(long long base, long long power) {
     }
     return result;
 }
+
+
+// 蓄水池采样
+/*
+假设需要采样的数量为 k
+首先构建一个可容纳 k 个元素的数组，将序列的前 k 个元素放入数组中。
+然后对于第 j（j > k）个元素开始，以 k / j 的概率来决定该元素是否被替换到数组中（数组中的k个元素被替换的概率是相同的）。 
+当遍历完所有元素之后，数组中剩下的元素即为所需采取的样本。
+
+证明：
+定理：该算法保证每个元素以 k / n 的概率被选入蓄水池数组。
+
+证明：首先，对于任意的 i，第 i 个元素进入蓄水池的概率为 k / i；而在蓄水池内每个元素被替换的概率为 1 / k; 
+因此在第 i 轮第j个元素被替换的概率为 (k / i ) * (1 / k) = 1 / i。 接下来用数学归纳法来证明，当循环结束时每个元素进入蓄水池的概率为 k / n.
+
+假设在 (i-1) 次迭代后，任意一个元素进入 蓄水池的概率为 k / (i-1)。有上面的结论，在第 i 次迭代时，该元素被替换的概率为 1 / i， 
+那么其不被替换的概率则为 1 - 1/i = (i-1)/i；在第i 此迭代后，该元素在蓄水池内的概率为 k / (i-1) * (i-1)/i = k / i. 归纳部分结束。
+
+因此当循环结束时，每个元素进入蓄水池的概率为 k / n. 命题得证。
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+/** 
+ * Reservoir Sampling Algorithm
+ * 
+ * Description: Randomly choose k elements from n elements ( n usually is large
+ *              enough so that the full n elements may not fill into main memory)
+ * Parameters:
+ *      v - the original array with n elements
+ *      n - the length of v
+ *      k - the number of choosen elements
+ * 
+ * Returns:
+ *      An array with k elements choosen from v
+ *
+ * Assert: 
+ *      k <= n
+ *
+ */
+vector<int> ReservoirSampling(vector<int> v, int n, int k) {
+
+    assert(v.size() == n && k <= n);
+
+    // init: fill the first k elems into reservoir
+    vector<int> reservoirArray(v.begin(), v.begin() + k);
+
+    int i = 0;
+    int j = 0;
+    // start from the (k+1)th element to replace
+    for (i = k; i < n; ++ i) {
+        j = rand() % (i + 1); // inclusive range [0, i]
+        // 被替换的概率是 1 / i
+        if (j < k) {
+            reservoirArray[j] = v[i];
+        }
+    }
+
+    return reservoirArray;
+}
+
+int main() {
+    vector<int> v(100, 0);
+    for (int i = 0; i < 100; ++ i) v[i] = i + 1;
+
+    srand((unsigned int)time(NULL));
+    // test algorithm RUN_COUNT times
+    const int RUN_COUNT = 10000;
+    int cnt[11] = {0};
+    for (int k = 1; k <= RUN_COUNT; ++k) {
+        cout << "Running Count " << k << endl;
+
+        vector<int> samples = ReservoirSampling(v, 100, 10);
+
+        for (size_t i = 0; i <samples.size(); ++i) {
+            cout << samples[i] << " ";
+            cnt[samples[i]] ++;
+        }
+        cout << endl;
+    }
+
+    // output frequency stats
+    cout << "*************************" << endl;
+    cout << "Frequency Stats" << endl;
+    for (int num = 1; num < 11; ++ num) {
+        cout << num << " : \t" << cnt[num] << endl;
+    }
+    cout << "*************************" << endl;
+
+    return 0;
+}
